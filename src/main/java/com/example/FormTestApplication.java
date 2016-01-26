25f895f6e2554dbfdc9c701b3c5568074905f4ef
Marcin Marczyk <marcin.marczyk@capgemini.com>
2016-01-26 16:10:00 +0100

    initialized git repo

---
+++ src/main/java/com/example/FormTestApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormTestApplication.class, args);
+	}
+}
